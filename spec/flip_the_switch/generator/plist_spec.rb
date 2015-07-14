807e24c12b34bb8e84d138c6667be2252776070e
Vincent Garrigues <vincent.garrigues@gmail.com>
2015-07-14 14:28:28 +0200

    plist -o can be either a file or a directory
    Allows specifying the name of the outputted plist

--- spec/flip_the_switch/generator/plist_spec.rb
+++ spec/flip_the_switch/generator/plist_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe FlipTheSwitch::Generator::Plist do
   subject(:plist) { described_class.new(output, features) }
-  let(:output) { 'tmp' }
   let(:features) { [
     FlipTheSwitch::Feature.new('enabled_feature', true, nil),
     FlipTheSwitch::Feature.new('disabled_feature', false, 'is disabled description')
@@ -15,9 +14,25 @@ describe FlipTheSwitch::Generator::Plist do
     File.delete(output_file) if File.exists?(output_file)
   end
 
-  it 'writes a Features.plist file with the enabled features set' do
-    subject.generate
+  context 'When the output is a directory' do
+    let(:output) { 'spec/resources' }
+    let(:output_file) { 'spec/resources/Features.plist' }
 
-    expect(actual_output_file).to eql(expected_plist_file)
+    it 'writes a Features.plist file with the enabled features set' do
+      subject.generate
+
+      expect(File.read(output_file)).to eql(expected_plist_file)
+    end
+  end
+
+  context 'when the output is a file' do
+    let(:output) { 'spec/resources/DasPlist.plist' }
+    let(:output_file) { 'spec/resources/DasPlist.plist' }
+
+    it 'writes a DasPlist.plist file with the enabled features set' do
+      subject.generate
+
+      expect(File.read(output_file)).to eql(expected_plist_file)
+    end
   end
 end
