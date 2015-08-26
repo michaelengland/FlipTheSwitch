04d70c181e29f50bd9dd6286e683b09372119e5e
FrauR <kristina.roddewig@gmail.com>
2015-08-26 16:33:10 +0200

    remove 'nullable' attribute to stop failing build jobs

--- Classes/FlipTheSwitch/FTSFeature.h
+++ Classes/FlipTheSwitch/FTSFeature.h
@@ -1,15 +1,11 @@
 @import Foundation;
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface FTSFeature : NSObject
 @property (nonatomic, readonly) NSString *name;
-@property (nonatomic, readonly, nullable) NSString *featureDescription;
+@property (nonatomic, readonly) NSString *featureDescription;
 @property (nonatomic, readonly) BOOL enabled;
 
 - (id)init NS_UNAVAILABLE;
 - (instancetype)initWithName:(NSString *)name enabled:(BOOL)enabled featureDescription:(NSString *)featureDescription;
 
 @end
-
-NS_ASSUME_NONNULL_END
